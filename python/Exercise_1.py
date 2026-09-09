937d4c5b9fa1ae5b24d4b926c307444ec3f47593
Andykharboe <andykharboe@gmail.com>
2026-09-09 09:43:30 +0200

    A little cleanup

--- python/Exercise_1.py
+++ python/Exercise_1.py
@@ -20,19 +20,18 @@ def rotate_inplace(arlo:Robot, degrees=90.0, turn_left=True):
         sleep(degrees * sec_pr_degree)
     print(arlo.stop())
 
-def square(arlo:Robot, side_length:1.0, turn_left=True):
-    # 1
-    straight_ahead(arlo, side_length)
-    rotate_inplace(arlo, turn_left=turn_left)
-    # 2
-    straight_ahead(arlo, side_length)
-    rotate_inplace(arlo, turn_left=turn_left)
-    # 3
-    straight_ahead(arlo, side_length)
-    rotate_inplace(arlo, turn_left=turn_left)
-    # 4
-    straight_ahead(arlo, side_length)
-    rotate_inplace(arlo, turn_left=turn_left)
+def square(arlo:Robot, side_length=1.0, turn_left=True, angle=90.0, old=False):
+    if old:
+    	for _ in range(4):
+            print(arlo.go_diff(60, 64, 1,1))
+            sleep(2.5)
+            print(arlo.go_diff(61, 64, 0,1))
+            sleep(0.7975)
+            sleep(0.1)
+    else:
+        for _ in range(4):
+            straight_ahead(arlo, side_length)
+            rotate_inplace(arlo, degrees=angle, turn_left=turn_left)
 
 
 
@@ -40,8 +39,8 @@ def square(arlo:Robot, side_length:1.0, turn_left=True):
 
 def continuous(arlo:Robot, n=1):
     for _ in range(n):
-        print(arlo.go_diff(45, 90, 1, 1))
+        print(arlo.go_diff(55, 100, 1, 1))
         sleep(7)
-        print(arlo.go_diff(90,45, 1, 1))
+        print(arlo.go_diff(100,55, 1, 1))
         sleep(7)
     print(arlo.stop())
