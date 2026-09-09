473c3a75c0774a139ad9e331e5f475d3750f6f64
Andykharboe <andykharboe@gmail.com>
2026-09-09 15:08:54 +0200

    fix

--- python/Exercise_2_1-ver2.py
+++ python/Exercise_2_1-ver2.py
@@ -75,7 +75,7 @@ while start<20: #kører i 20 sekunder
         arlo.go_diff(leftSpeed, rightSpeed, 1, 1)
         obstacle_avoidance(arlo, leftSpeed, rightSpeed, safe_distance)
         
-
+arlo.stop()
 
 
     
