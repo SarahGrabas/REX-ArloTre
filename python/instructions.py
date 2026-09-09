90ea9fec81b6eb0cf4c4b82b38e2132cb58d536f
Andykharboe <andykharboe@gmail.com>
2026-09-09 11:05:30 +0200

    fix

--- python/instructions.py
+++ python/instructions.py
@@ -9,4 +9,4 @@ while input("\nDo action? [y/n] ").lower() == "y":
 
     E1.square(arlo, turn_left=True, angle=float(input("angle=")))
 
-    # E1.continuous(arlo, n=input("n="), swap=input("swap="), times=(float(input("l=")),float(input("r="))))
+    E1.continuous(arlo, n=int(input("n=")), swap=bool(int(input("swap="))), times=(float(input("l=")),float(input("r="))))
