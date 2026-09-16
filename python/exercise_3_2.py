3b3226bc46c1ad1efb9762cc87114153af583330
Andykharboe <andykharboe@gmail.com>
2026-09-16 14:01:54 +0200

    fix

--- python/exercise_3_2.py
+++ python/exercise_3_2.py
@@ -1,17 +1,33 @@
 import cv2
 import cv2.aruco as aruco
 import numpy as np
+import picamera2
+from robot import arlo
+
+# Camera setup
+cam = picamera2.Picamera2() # Open camera
+config = cam.create_video_configuration( # define camera config
+    {"size": (1640, 1232), "format": "RGB888"}
+)
+cam.configure(config) # use config
+cam.start(show_preview=False) # start camera (turn on)
+time.sleep(1) # wait for camera to start
+
+
+def take_picture():
+    """Takes image in RBG format and return array of shape (hight, width, rbg)"""
+    return cam.capture_array("main")
 
 # 1. Kamerakalibrering (fra opgave 1)
-focal_length = 800.0  # Eksempel i pixels
-cx, cy = 320.0, 240.0  # Eksempel for 640x480 resolution
+focal_length = 12.889
+cx, cy = 1640/2, 1232/2 # camera center in pixels
 camera_matrix = np.array(
     [[focal_length, 0, cx], [0, focal_length, cy], [0, 0, 1]], dtype=np.float32
 )
 dist_coeffs = np.zeros((5, 1), dtype=np.float32)
 
-marker_length = 0.10  # Markørstørrelse i meter (f.eks. 10 cm)
-target_id = 42  # Specifikt ID eller None for enhver markør
+marker_length = 0.146  # Markørstørrelse i meter
+target_id = 4  # Specifikt ID eller None for enhver markør
 
 # ArUco Opsætning
 dictionary = aruco.getPredefinedDictionary(aruco.DICT_6X6_250)
@@ -22,7 +38,7 @@ state = "SEARCHING"
 lost_frames = 0
 
 while True:
-    frame = capture_image_from_robot()  # Hent frame fra PiCamera2
+    frame = take_picture()  # Hent frame fra PiCamera2
     gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
 
     corners, ids, rejected = aruco.detectMarkers(
