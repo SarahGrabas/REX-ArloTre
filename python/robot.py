79b9ce1f5eea7dd30f6ff44337a2ad7418996950
Andykharboe <andykharboe@gmail.com>
2026-09-21 16:04:13 +0200

    calib fix4

--- python/robot.py
+++ python/robot.py
@@ -140,7 +140,7 @@ class Robot(object):
         
         lower, middle, upper = _range
                 
-        for _ in range(n * 300//gcd(degrees, 360)):
+        for _ in range(n * 360//gcd(degrees, 360)):
             sleep(wait)
             arlo.go_diff(speed*left_wheel, speed*(not left_wheel), forward_drive, forward_drive)
             sleep(middle)
@@ -177,7 +177,7 @@ Enter adjustment action [?/-/0/+/<Enter>]:
                 print("Invalid input!")
                 continue
                 
-            for _ in range(n * 300//gcd(degrees, 360)):
+            for _ in range(n * 360//gcd(degrees, 360)):
                 sleep(wait)
                 arlo.go_diff(speed*left_wheel, speed*(not left_wheel), forward_drive, forward_drive)
                 sleep(middle)
@@ -190,7 +190,7 @@ Enter adjustment action [?/-/0/+/<Enter>]:
 
         lower, middle, upper = _range
                 
-        for _ in range(n * 300//gcd(degrees, 360)):
+        for _ in range(n * 360//gcd(degrees, 360)):
             sleep(wait)
             arlo.go_diff(middle*left_wheel, middle*(not left_wheel), forward_drive, forward_drive)
             sleep(_sleep)
@@ -227,7 +227,7 @@ Enter adjustment action [?/-/0/+/<Enter>]:
                 print("Invalid input!")
                 continue
                 
-            for _ in range(n * 300//gcd(degrees, 360)):
+            for _ in range(n * 360//gcd(degrees, 360)):
                 sleep(wait)
                 arlo.go_diff(middle*left_wheel, middle*(not left_wheel), forward_drive, forward_drive)
                 sleep(_sleep)
