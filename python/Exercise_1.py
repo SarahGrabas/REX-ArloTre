db1131909781b98a30e5e27bab2f2c7bf96aa530
Andykharboe <andykharboe@gmail.com>
2026-09-21 15:52:34 +0200

    calib fix

--- python/Exercise_1.py
+++ python/Exercise_1.py
@@ -72,226 +72,226 @@ Enter number for desired action [1/2/3/4/<Enter>] :
 [4] Eight shape (continuous)
 <Enter> Terminate
 > """)
-        match userinput:
-            case '0':
-                print("Calibration")
+        
+        if userinput == '0':
+            print("Calibration")
 
-                c0_left_wheel = True
-                c0_forward_drive = True
-                c0_n = 1
-                c0_degrees = 90
-                c0_speed = 40
-                c0_wait = 0.5
+            c0_left_wheel = True
+            c0_forward_drive = True
+            c0_n = 1
+            c0_degrees = 90
+            c0_speed = 40
+            c0_wait = 0.5
 
-                print("Configure parameters (press <Enter> for default):")
-                while True:
-                    userinput = input(f"> left_wheel (bool | default {c0_left_wheel}) = ")
-                    try:
-                        c0_left_wheel = bool(userinput)
+            print("Configure parameters (press <Enter> for default):")
+            while True:
+                userinput = input(f"> left_wheel (bool | default {c0_left_wheel}) = ")
+                try:
+                    c0_left_wheel = bool(userinput)
+                    break
+                except:
+                    print("Invalid input!")
+                    continue
+            while True:
+                userinput = input(f"> forward_drive (bool | default {c0_forward_drive}) = ")
+                try:
+                    c0_forward_drive = bool(userinput)
+                    break
+                except:
+                    print("Invalid input!")
+                    continue
+            while True:
+                userinput = input(f"> n (int | default {c0_n}) = ")
+                try:
+                    c0_n = int(userinput)
+                    if c0_n > 0: 
                         break
-                    except:
+                    else:
                         print("Invalid input!")
                         continue
-                while True:
-                    userinput = input(f"> forward_drive (bool | default {c0_forward_drive}) = ")
-                    try:
-                        c0_forward_drive = bool(userinput)
+                except:
+                    print("Invalid input!")
+                    continue
+            while True:
+                userinput = input(f"> degrees (int | default {c0_degrees}) = ")
+                try:
+                    c0_degrees = int(userinput)
+                    if c0_degrees > 0: 
                         break
-                    except:
+                    else:
                         print("Invalid input!")
                         continue
-                while True:
-                    userinput = input(f"> n (int | default {c0_n}) = ")
-                    try:
-                        c0_n = int(userinput)
-                        if c0_n > 0: 
-                            break
-                        else:
-                            print("Invalid input!")
-                            continue
-                    except:
-                        print("Invalid input!")
-                        continue
-                while True:
-                    userinput = input(f"> degrees (int | default {c0_degrees}) = ")
-                    try:
-                        c0_degrees = int(userinput)
-                        if c0_degrees > 0: 
-                            break
-                        else:
-                            print("Invalid input!")
-                            continue
-                    except:
-                        print("Invalid input!")
-                        continue
-                while True:
-                    userinput = input("> speed (int | range {0, [40;127]} "+f"| default {c0_speed}) = ")
-                    try:
-                        c0_speed = int(userinput)
-                        if Robot._power_checker(c0_speed): 
-                            break
-                        else:
-                            print("Invalid input!")
-                            continue
-                    except:
-                        print("Invalid input!")
-                        continue
-                while True:
-                    userinput = input(f"> wait (float | default {c0_wait}) = ")
-                    try:
-                        c0_wait = float(userinput)
-                        if c0_wait > 0: 
-                            break
-                        else:
-                            print("Invalid input!")
-                            continue
-                    except:
-                        print("Invalid input!")
-                        continue
-                
-                print(arlo._calibrate_sleep(c0_left_wheel, c0_forward_drive, c0_n, c0_degrees, c0_speed, wait=c0_wait))
-
-            case '1':
-                print("Straigt ahead:")
-
-                c1_meters = 1.0
-                print("Configure parameters (press <Enter> for default):")
-                while True:
-                    userinput = input(f"> meters (float | default {c1_meters}) = ")
-                    try:
-                        c1_meters = float(userinput)
-                        if c1_meters >= 0: 
-                            break
-                        else:
-                            print("Invalid input!")
-                            continue
-                    except:
-                        print("Invalid input!")
-                        continue
-                straight_ahead(arlo, c1_meters)
-
-            case '2':
-                print("Rotate inplace:")
-
-                c2_degrees = 90.0
-                c2_turn_left = True
-                print("Configure parameters (press <Enter> for default):")
-                while True:
-                    userinput = input(f"> degrees (float | default {c2_degrees}) = ")
-                    try:
-                        c2_degrees = float(userinput)
-                        if c2_degrees >= 0: 
-                            break
-                        else:
-                            print("Invalid input!")
-                            continue
-                    except:
-                        print("Invalid input!")
-                        continue
-                while True:
-                    userinput = input(f"> turn_left (bool | default {c2_turn_left}) = ")
-                    try:
-                        c2_turn_left = bool(userinput)
+                except:
+                    print("Invalid input!")
+                    continue
+            while True:
+                userinput = input("> speed (int | range {0, [40;127]} "+f"| default {c0_speed}) = ")
+                try:
+                    c0_speed = int(userinput)
+                    if Robot._power_checker(c0_speed): 
                         break
-                    except:
+                    else:
                         print("Invalid input!")
                         continue
-                rotate_inplace(arlo, c2_degrees, c2_turn_left)
-                
-            case '3':
-                print("Square shape:")
-
-                c3_side_length = 1.0,
-                c3_turn_left = True,
-                c3_angle = 90.0
-                print("Configure parameters (press <Enter> for default):")
-                while True:
-                    userinput = input(f"> side_length (float | default {c3_side_length}) = ")
-                    try:
-                        c3_side_length = float(userinput)
-                        if c3_side_length >= 0: 
-                            break
-                        else:
-                            print("Invalid input!")
-                            continue
-                    except:
-                        print("Invalid input!")
-                        continue
-                while True:
-                    userinput = input(f"> turn_left (bool | default {c3_turn_left}) = ")
-                    try:
-                        c3_turn_left = bool(userinput)
+                except:
+                    print("Invalid input!")
+                    continue
+            while True:
+                userinput = input(f"> wait (float | default {c0_wait}) = ")
+                try:
+                    c0_wait = float(userinput)
+                    if c0_wait > 0: 
                         break
-                    except:
+                    else:
                         print("Invalid input!")
                         continue
-                while True:
-                    userinput = input(f"> angle (float | default {c3_angle}) = ")
-                    try:
-                        c3_angle = float(userinput)
-                        if c3_angle >= 0: 
-                            break
-                        else:
-                            print("Invalid input!")
-                            continue
-                    except:
-                        print("Invalid input!")
-                        continue
-                square(arlo, c3_side_length, c3_turn_left, c3_angle)
+                except:
+                    print("Invalid input!")
+                    continue
+            
+            print(arlo._calibrate_sleep(c0_left_wheel, c0_forward_drive, c0_n, c0_degrees, c0_speed, wait=c0_wait))
 
-            case '4':
-                print("Eight shape (continuous):")
+        elif userinput == '1':
+            print("Straigt ahead:")
 
-                c4_n = 1,
-                c4_swap = False,
-                c4_left_sleep = 9.0
-                c4_right_sleep = 8.0
-                print("Configure parameters (press <Enter> for default):")
-                while True:
-                    userinput = input(f"> n (int | default {c4_n}) = ")
-                    try:
-                        c4_n = int(userinput)
-                        if c4_n > 0: 
-                            break
-                        else:
-                            print("Invalid input!")
-                            continue
-                    except:
-                        print("Invalid input!")
-                        continue
-                while True:
-                    userinput = input(f"> swap (bool | default {c4_swap}) = ")
-                    try:
-                        c4_swap = bool(userinput)
+            c1_meters = 1.0
+            print("Configure parameters (press <Enter> for default):")
+            while True:
+                userinput = input(f"> meters (float | default {c1_meters}) = ")
+                try:
+                    c1_meters = float(userinput)
+                    if c1_meters >= 0: 
                         break
-                    except:
+                    else:
                         print("Invalid input!")
                         continue
-                while True:
-                    userinput = input(f"> left_sleep (float | default {c4_left_sleep}) = ")
-                    try:
-                        c4_left_sleep = float(userinput)
-                        if c4_left_sleep >= 0: 
-                            break
-                        else:
-                            print("Invalid input!")
-                            continue
-                    except:
-                        print("Invalid input!")
-                        continue
-                while True:
-                    userinput = input(f"> right_sleep (float | default {c4_right_sleep}) = ")
-                    try:
-                        c4_right_sleep = float(userinput)
-                        if c4_right_sleep >= 0: 
-                            break
-                        else:
-                            print("Invalid input!")
-                            continue
-                    except:
-                        print("Invalid input!")
-                        continue
-                continuous(arlo, c4_n, c4_swap, (c4_left_sleep, c4_right_sleep))
+                except:
+                    print("Invalid input!")
+                    continue
+            straight_ahead(arlo, c1_meters)
 
-            case _:
-                break
+        elif userinput == '2':
+            print("Rotate inplace:")
+
+            c2_degrees = 90.0
+            c2_turn_left = True
+            print("Configure parameters (press <Enter> for default):")
+            while True:
+                userinput = input(f"> degrees (float | default {c2_degrees}) = ")
+                try:
+                    c2_degrees = float(userinput)
+                    if c2_degrees >= 0: 
+                        break
+                    else:
+                        print("Invalid input!")
+                        continue
+                except:
+                    print("Invalid input!")
+                    continue
+            while True:
+                userinput = input(f"> turn_left (bool | default {c2_turn_left}) = ")
+                try:
+                    c2_turn_left = bool(userinput)
+                    break
+                except:
+                    print("Invalid input!")
+                    continue
+            rotate_inplace(arlo, c2_degrees, c2_turn_left)
+            
+        elif userinput == '3':
+            print("Square shape:")
+
+            c3_side_length = 1.0,
+            c3_turn_left = True,
+            c3_angle = 90.0
+            print("Configure parameters (press <Enter> for default):")
+            while True:
+                userinput = input(f"> side_length (float | default {c3_side_length}) = ")
+                try:
+                    c3_side_length = float(userinput)
+                    if c3_side_length >= 0: 
+                        break
+                    else:
+                        print("Invalid input!")
+                        continue
+                except:
+                    print("Invalid input!")
+                    continue
+            while True:
+                userinput = input(f"> turn_left (bool | default {c3_turn_left}) = ")
+                try:
+                    c3_turn_left = bool(userinput)
+                    break
+                except:
+                    print("Invalid input!")
+                    continue
+            while True:
+                userinput = input(f"> angle (float | default {c3_angle}) = ")
+                try:
+                    c3_angle = float(userinput)
+                    if c3_angle >= 0: 
+                        break
+                    else:
+                        print("Invalid input!")
+                        continue
+                except:
+                    print("Invalid input!")
+                    continue
+            square(arlo, c3_side_length, c3_turn_left, c3_angle)
+
+        elif userinput == '4':
+            print("Eight shape (continuous):")
+
+            c4_n = 1,
+            c4_swap = False,
+            c4_left_sleep = 9.0
+            c4_right_sleep = 8.0
+            print("Configure parameters (press <Enter> for default):")
+            while True:
+                userinput = input(f"> n (int | default {c4_n}) = ")
+                try:
+                    c4_n = int(userinput)
+                    if c4_n > 0: 
+                        break
+                    else:
+                        print("Invalid input!")
+                        continue
+                except:
+                    print("Invalid input!")
+                    continue
+            while True:
+                userinput = input(f"> swap (bool | default {c4_swap}) = ")
+                try:
+                    c4_swap = bool(userinput)
+                    break
+                except:
+                    print("Invalid input!")
+                    continue
+            while True:
+                userinput = input(f"> left_sleep (float | default {c4_left_sleep}) = ")
+                try:
+                    c4_left_sleep = float(userinput)
+                    if c4_left_sleep >= 0: 
+                        break
+                    else:
+                        print("Invalid input!")
+                        continue
+                except:
+                    print("Invalid input!")
+                    continue
+            while True:
+                userinput = input(f"> right_sleep (float | default {c4_right_sleep}) = ")
+                try:
+                    c4_right_sleep = float(userinput)
+                    if c4_right_sleep >= 0: 
+                        break
+                    else:
+                        print("Invalid input!")
+                        continue
+                except:
+                    print("Invalid input!")
+                    continue
+            continuous(arlo, c4_n, c4_swap, (c4_left_sleep, c4_right_sleep))
+
+        else:
+            break
