90756ade62da1d7621e2059dd2c6da69a0940ec0
Andykharboe <andykharboe@gmail.com>
2026-09-23 16:50:25 +0200

    idk

--- python/Exercise_4_1.py
+++ python/Exercise_4_1.py
@@ -32,8 +32,12 @@ map_array = [(
     BOX_CIRKEL_RADIUS) 
     for id,(x,y,z) in zip(ids,tvecs)]
 
+print(rvecs, tvecs)
+
 # Note:
 # rvecs, tvecs
-# [[ 3.11429633 -0.0338186   0.01586285]] [[0.01003483 0.04807003 0.37233776]]   ; looking right on 
-# [[ 2.93820187 -0.02655488 -1.08660292]] [[-0.08554608  0.05370342  0.50980161]]   ; 45 degrees (2 faces visible)
+# [[ 3.11429633 -0.0338186   0.01586285]] [[0.01003483 0.04807003 0.37233776]]   ; boksen er lige frem, ikke roteret
+# [[-0.01582608 -3.13767396  0.13222295]] [[-0.00227703  0.05177388  0.52987108]]   ; boksen er lige frem, på hovedet, ikke roteret
+# [[ 2.93820187 -0.02655488 -1.08660292]] [[-0.08554608  0.05370342  0.50980161]]   ; boksen er lige frem, roteret 45 grader (2 sider synlige)
+# [[ 2.1736008  -2.21441669  0.08033051]] [[0.00755181 0.095368   0.47405852]]   ; boksen er lige frem, væltet 90 grader
 
