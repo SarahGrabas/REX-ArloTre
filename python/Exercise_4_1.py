837b02635d1606a97e2f39828c67a9d98f7884c5
Andykharboe <andykharboe@gmail.com>
2026-09-23 15:30:12 +0200

    test

--- python/Exercise_4_1.py
+++ python/Exercise_4_1.py
@@ -53,6 +53,6 @@ ids, rvecs, tvecs = arlo.picDetectMarkersPose()
 # +Y : Down
 # +Z : Front
 
-print([CAMERA_TO_ROBOT @ np.array([x,y,z,1]) for r,(x,y,z) in zip(rvecs,tvecs)])
-# print([CAMERA_TO_ROBOT @ (rotate(r, np.array([0,0,BOX_SIZE/2,0])) + np.array([x,y,z,1])) for r,(x,y,z) in zip(rvecs,tvecs)])
+# print([CAMERA_TO_ROBOT @ np.array([x,y,z,1]) for r,(x,y,z) in zip(rvecs,tvecs)])
+print([CAMERA_TO_ROBOT @ (rotate(r, np.array([0,0,BOX_SIZE/2,0])) + np.array([x,y,z,1])) for r,(x,y,z) in zip(rvecs,tvecs)])
 
