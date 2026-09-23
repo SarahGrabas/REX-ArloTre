7ce28b2b2248998876923688c1dff42530fb0d4f
Andykharboe <andykharboe@gmail.com>
2026-09-23 09:53:54 +0200

    calib

--- python/instructions.py
+++ python/instructions.py
@@ -11,12 +11,12 @@ from robot import arlo
 # left, forward, deg 360, 8 sec, 51
 # left, backward, deg 360, 8 sec, 51
 # right, forward, deg 360, 8 sec, 57
-# right, backward, deg 360, 8 sec, 57
+# right, backward, deg 360, 8 sec, 53
 
 while True:
     usr = input("speed = ")
     for _ in range(3):
         sleep(0.5)
-        arlo.go_diff(0, int(usr), 0, 0)
+        arlo.go_diff(int(usr), 0, 0, 0)
         sleep(8)
         arlo.stop()
