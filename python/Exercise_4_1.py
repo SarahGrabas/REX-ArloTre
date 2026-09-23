e6e230ea09ddbd1fe8feb75d174af8ebfa1db078
Andykharboe <andykharboe@gmail.com>
2026-09-23 15:18:25 +0200

    test2

--- python/Exercise_4_1.py
+++ python/Exercise_4_1.py
@@ -3,12 +3,41 @@ import numpy as np
 
 ROBOT_TO_CAMERA = np.array([
     [1, 0, 0, 0],
-    [0, 0, -1, 0.225],
+    [0, 0, -1, -0.225],
     [0, 1, 0, 0.208],
     [0, 0, 0, 1]
 ])
 
 CAMERA_TO_ROBOT = np.linalg.inv(ROBOT_TO_CAMERA)
+
+BOX_SIZE = 0.27
+
+def rotate(r, v):
+    """Returns v rotated with r (r is the axis-angle rotation representation)"""
+    v = np.asarray(v, dtype=float)
+    r = np.asarray(r, dtype=float)
+
+    theta = np.linalg.norm(r)
+
+    if theta < 1e-12:
+        return v.copy()
+
+    axis = r / theta
+    xyz = v[:3]
+
+    rotated = (
+        xyz * np.cos(theta)
+        + np.cross(axis, xyz) * np.sin(theta)
+        + axis * np.dot(axis, xyz) * (1 - np.cos(theta))
+    )
+
+    if len(v) == 4:
+        return np.append(rotated, v[3])
+    elif len(v) == 3:
+        return rotated
+    else:
+        raise ValueError("v skal have 3 eller 4 komponenter")
+
 
 arlo.start_camera()
 
@@ -24,5 +53,5 @@ ids, rvecs, tvecs = arlo.picDetectMarkersPose()
 # +Y : Down
 # +Z : Front
 
-print([CAMERA_TO_ROBOT @ np.array([x,y,z,1]) for x,y,z in tvecs])
+print([CAMERA_TO_ROBOT @ (rotate(r, np.array(0,0,BOX_SIZE/2,1)) + np.array([x,y,z,1])) for r,(x,y,z) in zip(rvecs,tvecs)])
 
